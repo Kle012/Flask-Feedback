79225b09f4afe673f7328207cb667af67594c77f
Kle012 <kimhle.0102@gmail.com>
2023-01-31 19:09:20 -0800

    Routes

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "Secret"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
