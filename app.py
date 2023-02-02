cba16f39ac2ea22e65a016ee2b97116dc9c276ff
Kle012 <kimhle.0102@gmail.com>
2023-02-01 16:57:25 -0800

    Routes

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, redirect, render_template, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
+from forms import AddRegisterForm
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "Secret"
@@ -19,3 +20,19 @@ with app.app_context():
 def redirect():
     return redirect ('/register')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register_user():
+    """
+    Show a register/create a user form which accept a username, password, email, first_name, last_name.
+    Process the registration form by adding a new user. Then redirect to /secret.
+    """
+    form = AddRegisterForm()
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != 'csrf_token'}
+        user = User(**data)
+        db.session.add(user)
+        db.session.commit()
+        flash (f'User {user.username} created.')
+        return redirect ('/secret')
+    else:
+        return render_template('register_form.html', form = form)
