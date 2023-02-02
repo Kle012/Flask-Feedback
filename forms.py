dd499ed67cea45203c88f159a6771966c7afcb80
Kle012 <kimhle.0102@gmail.com>
2023-02-01 17:55:50 -0800

    Forms

--- forms.py
+++ forms.py
@@ -2,10 +2,15 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email
 
-class AddRegisterForm (FlaskForm):
+class RegisterForm (FlaskForm):
     """Form to create a user."""
     username = StringField('Username', validators = [InputRequired(), Length(max=20)])
     password = PasswordField('Password', validators = [InputRequired()])
     email = StringField('Email', validators = [InputRequired(), Email(), Length(max=50)])
     first_name = StringField('First Name', validators = [InputRequired(), Length(max=30)])
     last_name = StringField('Last Name', validators = [InputRequired(), Length(max=30)])
+
+class LoginForm (FlaskForm):
+    """Form for user to log in."""
+    username = StringField('Username', validators = [InputRequired(), Length(max=20)])
+    password = PasswordField('Password', validators = [InputRequired()])
