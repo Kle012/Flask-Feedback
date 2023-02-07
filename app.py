9538213fcb741550b33916522cb6bc7aec650321
Kle012 <kimhle.0102@gmail.com>
2023-02-06 17:46:06 -0800

    Add Heroku configs

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import Unauthorized
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///feedback')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret')
